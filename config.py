4256384163f9495aaebf84fa9cd7cd868916c3e9
Noble Mungu <mungunoble@gmail.com>
2020-09-23 15:35:43 +0300

    deployment

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+class Config:
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:azbycx567@localhost/pitchperfect'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    SECRET_KEY ='wertyuiuytre'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+
+    '''
+    ProdConfig
+    '''
+    DEBUG = True
+
+class DevConfig(Config):
+    pass
+    
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
