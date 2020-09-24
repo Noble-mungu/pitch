fb2b8fecdd0f25e7ccbe3a81d4d538adeb0a0484
Noble Mungu <mungunoble@gmail.com>
2020-09-24 13:48:36 +0300

    deployment

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 class Config:
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:azbycx567@localhost/pitchperfect'
+    postgres://odvwihklcvlsfx:0b63486d69f81a4b35556aee4cbd8f707dc1dfb7f35eeec2a0483d7d5d51d80b@ec2-3-224-97-209.compute-1.amazonaws.com:5432/d1ip0kuc1sm40h
     SQLALCHEMY_TRACK_MODIFICATIONS=False
-    SECRET_KEY ='wertyuiuytre'
+    SECRET_KEY ='WOOWEDNESDAY'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
